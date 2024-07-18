72e509b55f292a448987ffd365f9edc046bfab6a
Piyush Rajput <168125085+Piyush000789@users.noreply.github.com>
2024-07-18 17:39:50 +0530

    Add files via upload

---
+++ CodSoft2/script.js
@@ -0,0 +1,15 @@
+    function appendNumber(number) {
+    document.getElementById('result').value += number;
+  }
+  
+  function appendOperator(operator) {
+    document.getElementById('result').value += operator;
+  }
+  
+  function calculate() {
+    var result = eval(document.getElementById('result').value);
+    document.getElementById('result').value = result;
+  }
+  function clearResult() {
+    document.getElementById('result').value = '';
+  }
